aead4316030c6103695461086545baab9f966eb4
Silas <u15231748@tuks.co.za>
2023-01-09 15:57:21 +0200

    Tetris

--- src/app/components/playground/playground.component.ts
+++ src/app/components/playground/playground.component.ts
@@ -12,40 +12,23 @@ type validKeys = "Space" | "ArrowLeft" | "ArrowRight" | "ArrowDown" | null
 export class PlaygroundComponent implements OnInit, AfterViewInit {
   private playgroundRef!: HTMLDivElement;
   private currentShape!: IPosition | null;
-  private ROW_SIZE: number = 25;
+  private ROW_SIZE: number = 23;
   private COLUMN_SIZE: number = 20;
   private movementDuration: number = 500;
   private timer!: NodeJS.Timeout;
+  private isKeyed!: Boolean;
 
   @ViewChild('playgroundContainer', { static: true })
   playgound!: ElementRef
 
   constructor(private blocks: BlockService) { }
 
-  public startGame(): void {
-    this.currentShape = this.blocks.generateBlockShape();
-
-    this.timer = setInterval(() => {
-      if (this.blocks.move(this.currentShape!)) {
-        // Reset current object
-        this.currentShape = null;
-
-        // Clearing the Interval
-        this.stopGame();
-
-        // Restarting the Interval
-        this.startGame();
-      }
-    }, this.movementDuration)
-  }
-
-  public stopGame(): void {
-    clearInterval(this.timer);
-  }
-
   ngAfterViewInit(): void {
     this.playgroundRef = this.playgound.nativeElement;
     this.blocks.drawBlockMatrix(this.playgroundRef, this.ROW_SIZE, this.COLUMN_SIZE);
+    this.currentShape = this.blocks.generateBlockShape();
+    this.isKeyed = false;
+
     this.startGame();
   }
 
@@ -54,6 +37,7 @@ export class PlaygroundComponent implements OnInit, AfterViewInit {
       if ((event.code === "Space" || event.code === "ArrowLeft"
         || event.code === "ArrowRight" || event.code === "ArrowDown")
         && (this.currentShape !== null)) {
+        this.isKeyed = true;
         switch (event.code) {
           case "Space":
             this.blocks.transform(this.currentShape)
@@ -70,5 +54,33 @@ export class PlaygroundComponent implements OnInit, AfterViewInit {
         }
       }
     })
+  }
+
+  public blockComp(): void {
+    if (!this.isKeyed) {
+      if (this.blocks.move(this.currentShape!)) {
+        // Reset current object
+        this.currentShape = null;
+        this.currentShape = this.blocks.generateBlockShape();
+        // console.log("Inner😎😋😊...", this.currentShape)
+        // return;
+        // Clearing the Interval
+        // this.stopGame();
+
+        // Restarting the Interval
+        // this.startGame();
+      }
+
+      this.stopGame();
+      this.startGame();
+    } else console.log("Keyed....😎😴😴")
+  }
+
+  public startGame(): void {
+    this.timer = setTimeout(this.blockComp.bind(this), this.movementDuration);
+  }
+
+  public stopGame(): void {
+    clearInterval(this.timer);
   }
 }
